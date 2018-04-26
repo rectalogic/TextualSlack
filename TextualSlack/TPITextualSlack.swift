f2d9b368c262a34e397e51fce943252f139778a8
Andrew Wason <rectalogic@rectalogic.com>
2018-04-26 17:24:42 -0400

    XCode generated project, tweaked for Textual plugin

---
+++ TextualSlack/TPITextualSlack.swift
@@ -0,0 +1,13 @@
+//
+//  TPITextualSlack.swift
+//  TextualSlack
+//
+//  Created by Andrew Wason on 4/26/18.
+//  Copyright © 2018 Andrew Wason. All rights reserved.
+//
+
+import Cocoa
+
+class TPITextualSlack: NSObject, THOPluginProtocol {
+
+}
